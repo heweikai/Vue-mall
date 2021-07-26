07a771029a224f868ec1845e43aedfd290dc2990
士多啤梨 <473062186@qq.com>
2021-07-26 22:46:04 +0800

    master

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="nav-bar">
+    <div class="left"><slot name="left"></slot></div>
+    <div class="center"><slot name="center"></slot></div>
+    <div class="right"><slot name="right"></slot></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<style>
+.nav-bar {
+  display: flex;
+  line-height: 44px;
+  height: 44px;
+  box-shadow: 0 5px 10px rgba(100, 100, 100, 0.1);
+}
+.left,
+.right {
+  width: 60px;
+}
+.center {
+  flex: 1;
+  text-align: center;
+  font-weight: 400;
+  font-size: 18px;
+  color: #dfe6e9;
+}
+</style>
---
+++ src/views/home/childComps/RecommendView.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="recommend">
+    <div v-for="item in recommends" :key="item" class="recommendItem">
+      <a :href="item.link">
+        <img :src="item.image" alt="" />
+      </a>
+      <div>{{ item.title }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RecommendView",
+  props: {
+    recommends: {
+      type: Array,
+      default() {
+        return [];
+      }
+    }
+  },
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<style>
+.recommend {
+  display: flex;
+  text-align: center;
+  width: 100%;
+  font-size: 12px;
+  padding: 10px 0 30px 0;
+  border-bottom: 8px solid #eee;
+}
+.recommendItem {
+  flex: 1;
+}
+.recommendItem img {
+  width: 70px;
+  margin-bottom: 10px;
+}
+</style>
